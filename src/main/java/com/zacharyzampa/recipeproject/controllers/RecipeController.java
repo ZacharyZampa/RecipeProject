2979ccb0e9eaffb9aeb2baeaab06e9c4ec989581
zacharyzampa <zackzampa@gmail.com>
2020-06-06 16:28:57 -0400

    Display the recipes that are on the site.

---
+++ src/main/java/com/zacharyzampa/recipeproject/controllers/RecipeController.java
@@ -0,0 +1,29 @@
+package com.zacharyzampa.recipeproject.controllers;
+
+import com.zacharyzampa.recipeproject.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 6/19/17.
+ */
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    // use {id} to grab the id out of the URL and then pass it into the method with @PathVariable
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+
+        return "recipe/show";  // return the view name of the template to use
+    }
+}
--- src/main/java/com/zacharyzampa/recipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/zacharyzampa/recipeproject/services/RecipeServiceImpl.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 
+import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
+
+@Slf4j
 @Service
-public class RecipeServiceImpl implements RecipeService{
+public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
 
@@ -18,8 +22,21 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long l) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
