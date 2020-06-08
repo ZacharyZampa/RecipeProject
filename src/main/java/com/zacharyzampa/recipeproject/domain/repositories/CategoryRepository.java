5910c96645d62f169829bbe3820d4ac07bdd2234
Zachary Zampa <zackzampa@gmail.com>
2020-06-08 07:12:42 -0400

    Update the command classes. Add a dropdown for difficulty for recipes

---
+++ src/main/java/com/zacharyzampa/recipeproject/domain/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.zacharyzampa.recipeproject.domain.repositories;
+
+
+import com.zacharyzampa.recipeproject.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+}
---
+++ src/main/java/com/zacharyzampa/recipeproject/domain/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,12 @@
+package com.zacharyzampa.recipeproject.domain.repositories;
+
+
+import com.zacharyzampa.recipeproject.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+}
